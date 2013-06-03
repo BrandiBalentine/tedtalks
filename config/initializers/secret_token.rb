3bce00f61a462bb94afe9508461e9536cdce2172
Brandi Balentine <brandibalentine@gmail.com>
2013-06-03 01:03:29 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tedtalks::Application.config.secret_token = 'd3dd769eb73af943f58ba1a11de70cfd28d52ac7f18049837b967938a7e0f772677a126e8254676170a69e69bc53feae8bf6dcb3f20ae9caa04395403d1ca078'
